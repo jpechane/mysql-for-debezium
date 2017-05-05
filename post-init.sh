32895e88ca40cf984e55320a567e3e792219a91e
Jiri Pechanec <jpechane@redhat.com>
2017-05-05 06:13:11 +0200

    Improve formatting

--- post-init.sh
+++ post-init.sh
@@ -1,4 +1,7 @@
-# Setup the 'master' replication on the MySQL server                                                                                                                                                                   
+# Setup the 'master' replication on the MySQL server
+
+echo Grant replication privileges
+
 mysql $mysql_flags <<EOSQL
   GRANT REPLICATION CLIENT, REPLICATION SLAVE, RELOAD ON *.* TO '${MYSQL_MASTER_USER}'@'%';
   FLUSH PRIVILEGES;
